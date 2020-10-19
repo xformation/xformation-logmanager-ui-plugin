286c6f01c9a563f418b13c8cd10637c3eb34a9e3
Satish pal <satishweb93@gmail.com>
2020-10-19 16:37:37 +0530

    UI design for allEventPopup and setOutputParameter modal

--- src/domain/Dashboard/allEventPopup.tsx
+++ src/domain/Dashboard/allEventPopup.tsx
@@ -9,7 +9,7 @@ export class AllEventsPopup extends React.Component<any, any> {
         super(props);
         this.state = {
             modal: false,
-
+            activeTab: 0,
         };
     }
 
@@ -24,48 +24,104 @@ export class AllEventsPopup extends React.Component<any, any> {
         });
     }
 
+    setActiveTab = (activeTab: any) => {
+        this.setState({
+            activeTab
+        });
+    };
+
 
     render() {
-        const { modal } = this.state;
+        const { modal, activeTab } = this.state;
         return (
             <Modal isOpen={modal} toggle={this.toggle} className="modal-container logmanager-modal-container">
                 <ModalHeader toggle={this.toggle}>All Events | <span>Manage Rules</span></ModalHeader>
                 <ModalBody style={{ height: 'calc(60vh - 50px)', overflowY: 'auto', overflowX: "hidden" }}>
                     <div className="d-block width-100 stream-popup-container">
-                        <div className="">
+                        <div className="message-tabs-container">
                             <span>Load a message to test Rule</span>
-                            <button>Recent Messages</button>
-                            <button>Message Id</button>
-                            <span>Select an input from the list below and click "Lod messages" to load the most recent message received by this input within the last hour.</span>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-12 col-md-12 col-sm-12">
-                                <div className="form-group">
-                                    <select>
-                                        <option value="">Select input</option>
-                                        <option value="abc">ABC</option>
-                                        <option value="def">DEF</option>
-                                        <option value="hij">HIJ</option>
-                                        <option value="klm">KLM</option>
-                                    </select>
+                            <div className="messages-tabs">
+                                <ul>
+                                    <li className={activeTab === 0 ? "active-tab" : ''} onClick={e => this.setActiveTab(0)}>
+                                        <a href="#">Recwnt Messages</a>
+                                    </li>
+                                    <li className={activeTab === 1 ? "active-tab" : ''} onClick={e => this.setActiveTab(1)}>
+                                        <a href="#">Message ID</a>
+                                    </li>
+                                </ul>
+                                <div className="tab-container">
+                                    {activeTab === 0 &&
+                                        <div>
+                                            <div className="lod-messages">Select an input from the list below and click "Lod messages" to load the most recent message received by this input within the last hour.</div>
+                                            <div className="row">
+                                                <div className="col-lg-12 col-md-12 col-sm-12">
+                                                    <div className="form-group">
+                                                        <select className="input-group-text">
+                                                            <option value="">Select input</option>
+                                                            <option value="abc">ABC</option>
+                                                            <option value="def">DEF</option>
+                                                            <option value="hij">HIJ</option>
+                                                            <option value="klm">KLM</option>
+                                                        </select>
+                                                        <button className="blue-button">Load Messages</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    }
+                                    {activeTab === 1 &&
+                                        <div>
+                                            <div className="lod-messages">Please provide the id and index of the message that you want to load in this form.</div>
+                                            <div className="row">
+                                                <div className="col-lg-12 col-md-12 col-sm-12">
+                                                    <div className="form-group">
+                                                        <select className="input-group-text">
+                                                            <option value="">Message ID</option>
+                                                            <option value="abc">ABC</option>
+                                                            <option value="def">DEF</option>
+                                                            <option value="hij">HIJ</option>
+                                                            <option value="klm">KLM</option>
+                                                        </select>
+                                                        <select className="input-group-text">
+                                                            <option value="">Index</option>
+                                                            <option value="abc">ABC</option>
+                                                            <option value="def">DEF</option>
+                                                            <option value="hij">HIJ</option>
+                                                            <option value="klm">KLM</option>
+                                                        </select>
+                                                        <button className="blue-button">Load Messages</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    }
                                 </div>
-                                <button>Load Messages</button>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-lg-12 col-md-12 col-sm-12">
-                                <h4>Manage Stream Rules</h4>
-                                <button>Add Rules</button>
+                        <div className="rules-defined">
+                            <div className="manage-rules">
+                                <div className="row">
+                                    <div className="col-lg-10 col-md-10 col-sm-12">
+                                        <h4>Manage Stream Rules</h4>
+                                        <span>Please load a message to check if it would match against these rules and therefore be routed into this stream.</span>
+                                    </div>
+                                    <div className="col-lg-2 col-md-2 col-sm-12">
+                                        <button className="blue-button">Add Rules</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-12 col-md-12 col-sm-12">
-                                <span>Please load a message to check if it would match against these rules and therefore be routed into this stream.</span>
+                            <div className="check-group">
+                                <input type="radio" value="1" name="gender" /> 
+                                <label>Stream containing all events created by synectics Log Managment</label>
                             </div>
-                        </div>
-                        <div className="">
-                            <input type="radio" value="1" name="gender" /> Stream containing all events created by synectics Log Managment
-                            <input type="radio" value="2" name="gender" /> Stream containing all events created by synectics Log Managment
+                            <div className="check-group">
+                                <input type="radio" value="2" name="gender" /> 
+                                <label>Stream containing all events created by synectics Log Managment</label>
+                            </div>
+                            <div className="no-rules-defined">
+                                <i className="fa fa-info-circle"></i>
+                                <span>No rules defined</span>
+                            </div>
                         </div>
                         <div className="row">
                             <div className="col-lg-12 col-md-12 col-sm-12">
--- src/domain/Dashboard/setOutputParameter.tsx
+++ src/domain/Dashboard/setOutputParameter.tsx
@@ -1,6 +1,6 @@
 import { Checkbox } from '@material-ui/core';
 import * as React from 'react';
-import { Modal, ModalBody } from 'reactstrap';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 
 
 export class SetOutputPopup extends React.Component<any, any> {
@@ -27,40 +27,55 @@ export class SetOutputPopup extends React.Component<any, any> {
     render() {
         const { modal } = this.state;
         return (
-            <Modal isOpen={modal} toggle={this.toggle} className="modal-container">
-                <button className="close-btn" onClick={this.handleClose}>X</button>
-                <ModalBody style={{ height: 'calc(75vh - 50px)', overflowY: 'auto', overflowX: "hidden" }}>
+            <Modal isOpen={modal} toggle={this.toggle} className="modal-container logmanager-modal-container">
+                <ModalHeader toggle={this.toggle}>All Events | <span>Manage Output</span></ModalHeader>
+                <ModalBody style={{ height: 'calc(60vh - 50px)', overflowY: 'auto', overflowX: "hidden" }}>
                     <div className="d-block width-100 stream-popup-container">
-                        <div className="d-block width-100 p-b-20 heading">
-                            <h4 className="d-block">All Events | Manage Output</h4>
-                        </div>
-                        <div className="">
+                        <div className="popup-parameters-container">
                             <span>Set your output Parameters here</span>
                             <div className="row">
-                                <div className="col-lg-3 col-md-3 col-sm-6">
-                                    <select>
-                                        <option value="">Select Output Type</option>
-                                        <option value="stdout">STDOUT Output</option>
-                                        <option value="gelf">GELF Outputs</option>
-                                    </select>
+                                <div className="col-lg-6 col-md-6 col-sm-6">
+                                    <div className="form-group">
+                                        <div className="row">
+                                            <div className="col-lg-7 col-md-7 col-sm-7">
+                                                <select className="input-group-text">
+                                                    <option value="">Select Output Type</option>
+                                                    <option value="stdout">STDOUT Output</option>
+                                                    <option value="gelf">GELF Outputs</option>
+                                                </select>
+                                            </div>
+                                            <div className="col-lg-5 col-md-5 col-sm-5 p-l-0 p-r-0">
+                                                <button className="blue-button m-r-0 m-b-0">Lunch New Output</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="col-lg-3 col-md-3 col-sm-6">
-                                    <button>Lunch New Output</button>
+                                <div className="col-lg-6 col-md-6 col-sm-6">
+                                    <div className="form-group">
+                                        <div className="row">
+                                            <div className="col-lg-7 col-md-7 col-sm-7">
+                                                <select className="input-group-text">
+                                                    <option value="">Select existing Output</option>
+                                                    <option value="">No output Available</option>
+                                                </select>
+                                            </div>
+                                            <div className="col-lg-5 col-md-5 col-sm-5 p-l-0 p-r-0">
+                                                <button className="blue-button m-r-0 m-b-0">Assign existing output</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="col-lg-3 col-md-3 col-sm-6">
-                                    <select>
-                                        <option value="">Select existing Output</option>
-                                        <option value="">No output Available</option>
-                                    </select>
-                                </div>
-                                <div className="col-lg-3 col-md-3 col-sm-6">
-                                    <button>Assign existing output</button>
+                            </div>
+                            <div className="rules-defined">
+                                <div className="no-rules-defined">
+                                    <i className="fa fa-info-circle"></i>
+                                    <span>No rules defined</span>
                                 </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-lg-12 col-md-12 col-sm-12">
-                                <div className="d-block text-center p-t-20 contact-popup-buttons">
+                                <div className="d-block text-right p-t-20 contact-popup-buttons">
                                     <button className="cancel" onClick={this.handleClose}>Cancel</button>
                                     <button className="save">Save</button>
                                 </div>
