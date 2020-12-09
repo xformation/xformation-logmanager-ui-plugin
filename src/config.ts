52bfdea01472e1e71549d4d470eb983f2f599d93
manojkrjoshi <manojkr.joshi@gmail.com>
2020-12-09 22:12:59 +0530

    stream get API changes

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-//const IP = "localhost";
-const IP = "100.64.108.25";
+const IP = "localhost";
+// const IP = "100.64.108.25";
 const securitySrvUrl = `http://${IP}:8094`;
 const catalogSrvUrl = `http://${IP}:5050/api`;
 const logMrgSrvUrl = `http://${IP}:9000/api`;
